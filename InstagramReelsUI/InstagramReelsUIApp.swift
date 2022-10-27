aa5248d3c28b5d7819a3426dad1069e75b768595
Tien Dao <tiendao5240@gmail.com>
2022-10-27 15:32:17 -0400

    Initial Commit

---
+++ InstagramReelsUI/InstagramReelsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramReelsUIApp.swift
+//  InstagramReelsUI
+//
+//  Created by Tien Dao on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct InstagramReelsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
