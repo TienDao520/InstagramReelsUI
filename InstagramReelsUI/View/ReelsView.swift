b1c3188ad3347683290f8b0d915c2d89f71e0c52
Tien Dao <tiendao5240@gmail.com>
2022-10-27 17:59:00 -0400

    Added Scrollable View

--- InstagramReelsUI/View/ReelsView.swift
+++ InstagramReelsUI/View/ReelsView.swift
@@ -12,12 +12,54 @@ struct ReelsView: View {
     @State var currentReel = ""
     
     var body: some View {
-        ///Vertical Page Tab View
-        TabView(selection: $currentReel){
-            ForEach(MediaFileJson){media in
-                Color.red
+        // Setting Width and height for rotated View
+        GeometryReader{proxy in
+            let size = proxy.size
+            ///Vertical Page Tab View
+            TabView(selection: $currentReel){
+                ForEach(MediaFileJson){media in
+                    VStack{
+                        Text("TienDao")
+                        
+                        Spacer()
+                        
+//                        ForEach(MediaFileJson){media in
+//                                VStack{
+//                                    Text("Hello")
+//
+//                                    Spacer()
+//
+//                                    Text("Hello")
+//                                        .frame(maxHeight: .infinity, alignment: .trailing)
+//                                        .foregroundColor(.red)
+//                                }
+//                                //setting width
+//                                .frame(width: size.width)
+//                                    .padding()
+//
+//                        }
+                       
+                        
+                        Text("Description")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                    }
+                    //setting width
+                    .frame(width: size.width)
                     .padding()
+                    //Rotating Content
+                    .rotationEffect(.init(degrees: -90))
+                    
+                    
+                }
             }
+            /// View Rotation
+            .rotationEffect(.init(degrees: 90))
+            /// Since view is rotated setting height as width
+            .frame(width: size.height)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            /// seting max width
+            .frame(width: size.width)
         }
     }
 }
