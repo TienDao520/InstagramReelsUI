5ed8825df996208c1cfd27e17ff2637d93f1f623
Tien Dao <tiendao5240@gmail.com>
2022-11-01 16:45:38 -0400

    Initial Commit

---
+++ VideoTestSwiftUI/VideoTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoTestSwiftUIApp.swift
+//  VideoTestSwiftUI
+//
+//  Created by Tien Dao on 2022-11-01.
+//
+
+import SwiftUI
+
+@main
+struct VideoTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
