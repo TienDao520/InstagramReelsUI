15a0978692fe4b288b167adcff5ea589483ad0b6
Tien Dao <tiendao5240@gmail.com>
2022-11-02 13:30:55 -0400

    Initial Commit

---
+++ TabViewCarousel/TabViewCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewCarouselApp.swift
+//  TabViewCarousel
+//
+//  Created by Tien Dao on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct TabViewCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
