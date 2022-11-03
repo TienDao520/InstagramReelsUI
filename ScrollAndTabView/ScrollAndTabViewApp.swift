ea14dbf2fd1834fb289a74fdcf4c39cdbde1d855
Tien Dao <tiendao5240@gmail.com>
2022-11-03 12:56:23 -0400

    Initial Commit

---
+++ ScrollAndTabView/ScrollAndTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollAndTabViewApp.swift
+//  ScrollAndTabView
+//
+//  Created by Tien Dao on 2022-11-03.
+//
+
+import SwiftUI
+
+@main
+struct ScrollAndTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
